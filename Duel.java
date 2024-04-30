1a96b7578e37f43d2e06bff57f4df11bf228b983
JrMatita <162496497+JrMatita@users.noreply.github.com>
2024-04-30 19:53:06 +0200

    Create Duel.java

---
+++ Duel.java
@@ -0,0 +1,17 @@
+package it.coralmc;
+
+import org.bukkit.entity.Player;
+
+public class Duel {
+    private Player player1;
+    private Player player2;
+    private String kitName;
+
+    public Duel(Player player1, Player player2, String kitName) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.kitName = kitName;
+      }
+
+
+}
