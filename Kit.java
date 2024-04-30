88763ca0385bf35b438a77a8717b574357d8e4e2
JrMatita <162496497+JrMatita@users.noreply.github.com>
2024-04-30 19:54:26 +0200

    Create Kit.java

---
+++ Kit.java
@@ -0,0 +1,21 @@
+package it.coralmc;
+
+import org.bukkit.inventory.ItemStack;
+
+public class Kit {
+    private String name;
+    private ItemStack[] items;
+
+    public Kit(String name, ItemStack[] items) {
+        this.name = name;
+        this.items = items;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ItemStack[] getItems() {
+        return items;
+    }
+}
