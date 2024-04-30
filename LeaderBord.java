3db5873c2e53c1b67bbdd5dc75cfd60cae1514a4
JrMatita <162496497+JrMatita@users.noreply.github.com>
2024-04-30 19:57:05 +0200

    Create LeaderBord.java

---
+++ LeaderBord.java
@@ -0,0 +1,22 @@
+package it.coralmc;
+
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class Leaderboard {
+    public List<Player> getTopPlayersByWins(int count) {
+
+        return null;
+    }
+
+    public List<Player> getTopPlayersByDuels(int count) {
+
+        return null;
+    }
+
+    public List<Player> getTopPlayersByLosses(int count) {
+
+        return null;
+    }
+}
